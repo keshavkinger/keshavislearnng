6b26489cafefae8f0b1cc5a4a9b6aec746d264f3
Keshav Kinger <keshavkinger@Keshavs-MacBook-Air-2.local>
2026-04-05 15:57:56 +0530

    added 2 lines

--- kinger.java
+++ kinger.java
@@ -1,5 +1,7 @@
 public class kinger {
     public static void main(String[]args){
         System.out.println("keshav ");
+        System.out.println("kinger");
+        System.out.println("Git practice");
     }
 }
